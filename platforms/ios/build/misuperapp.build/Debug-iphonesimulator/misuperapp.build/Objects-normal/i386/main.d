5ba9aad50e474f04616cc7790eb55b18deacb796
jorgeucano <jorgeucano@gmail.com>
2016-11-08 14:20:39 -0300

    remove folders

--- platforms/ios/build/misuperapp.build/Debug-iphonesimulator/misuperapp.build/Objects-normal/i386/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/jorgeucano/Documents/cursos/nativescript/code/mi-super-app/platforms/ios/internal/main.m \
-  /Users/jorgeucano/Documents/cursos/nativescript/code/mi-super-app/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
-  /Users/jorgeucano/Documents/cursos/nativescript/code/mi-super-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
-  /Users/jorgeucano/Documents/cursos/nativescript/code/mi-super-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
-  /Users/jorgeucano/Documents/cursos/nativescript/code/mi-super-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
-  /Users/jorgeucano/Documents/cursos/nativescript/code/mi-super-app/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/jorgeucano/Documents/cursos/nativescript/code/mi-super-app/platforms/ios/internal/TNSDebugging.h
--- platforms/ios/misuperapp/app/tns_modules/nativescript-angular/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./platform";
-export * from "./router";
-export * from "./forms";
-export * from "./http";
-export * from "./directives";
-export * from "./common/detached-loader";
